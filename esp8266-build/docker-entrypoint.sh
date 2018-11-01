5fbc0a0120b1ba7260301f134791aec341933f19
Piers Finlayson <piers@piersandkatie.com>
2018-11-01 20:48:02 +0000

    add version output when running container

--- esp8266-build/docker-entrypoint.sh
+++ esp8266-build/docker-entrypoint.sh
@@ -3,9 +3,12 @@
 set -e
 cd /home/esp/builds/
 if [ "$1" = 'make' ]; then
+  echo "SDK Version $(cat /VERSION)"
   cd $2 && make $3
 elif [ "$1" = 'bash' ]; then
+  echo "SDK Version $(cat /VERSION)"
   /bin/bash
 else
+  echo "SDK Version $(cat /VERSION)"
   exec "$@"
 fi
