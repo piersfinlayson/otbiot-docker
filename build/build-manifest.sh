2ff9e841ecfc3ee7c8d600e14645e059b8779c6c
Piers Finlayson <piers@piersandkatie.com>
2018-12-24 15:43:47 +0000

    implement separate arm versions properly

--- build/build-manifest.sh
+++ build/build-manifest.sh
@@ -8,7 +8,8 @@ echo "       version: ${VERSION}"
 docker login
 
 # Create VERSION manifest
-docker manifest create piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-arm:${VERSION}
+docker manifest create piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv7l:${VERSION}
+#docker manifest create piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv6l:${VERSION} piersfinlayson/build-armv7l:${VERSION}
 docker manifest annotate --arch amd64 --os linux piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION}
 #docker manifest annotate --arch arm --os linux --variant armv6l piersfinlayson/build:${VERSION} piersfinlayson/build-armv6l:${VERSION}
 docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/build:${VERSION} piersfinlayson/build-armv7l:${VERSION}
@@ -16,7 +17,8 @@ docker manifest inspect piersfinlayson/build:${VERSION}
 docker manifest push --purge piersfinlayson/build:${VERSION}
 
 # Create latest manifest
-docker manifest create piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-arm:${VERSION}
+docker manifest create piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv7l:${VERSION}
+#docker manifest create piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv6l:${VERSION} piersfinlayson/build-armv7l:${VERSION}
 docker manifest annotate --arch amd64 --os linux piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION}
 #docker manifest annotate --arch arm --os linux --variant armv6l piersfinlayson/build:latest piersfinlayson/build-armv6l:${VERSION}
 docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/build:latest piersfinlayson/build-armv7l:${VERSION}
