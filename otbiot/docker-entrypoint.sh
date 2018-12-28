cee8a3eb828ea29fe49752f4a916d09133ba6011
Piers Finlayson <piers@piersandkatie.com>
2018-12-28 20:43:09 +0000

    add otbiot

---
+++ otbiot/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+cd /home/esp/otb-iot/
+if [ "$1" = 'make' ]; then
+  echo "SDK Version $(cat /VERSION)"
+  cd $2 && make $3
+elif [ "$1" = 'bash' ]; then
+  echo "SDK Version $(cat /VERSION)"
+  /bin/bash
+else
+  echo "SDK Version $(cat /VERSION)"
+  exec "$@"
+fi
