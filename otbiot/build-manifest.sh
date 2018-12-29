88a38e06d35c53f630c956d458e957dd6d6e342b
Piers Finlayson <piers@piersandkatie.com>
2018-12-29 16:59:26 +0000

    remove duplicate line

--- otbiot/build-manifest.sh
+++ otbiot/build-manifest.sh
@@ -8,7 +8,6 @@ echo "       version: ${VERSION}"
 docker login
 
 # Create VERSION manifest
-docker manifest create piersfinlayson/otbiot:${VERSION} piersfinlayson/otbiot-amd64:${VERSION} piersfinlayson/otbiot-armv7l:${VERSION}
 docker manifest create piersfinlayson/otbiot:${VERSION} piersfinlayson/otbiot-amd64:${VERSION} piersfinlayson/otbiot-armv6l:${VERSION} piersfinlayson/otbiot-armv7l:${VERSION}
 docker manifest annotate --arch amd64 --os linux piersfinlayson/otbiot:${VERSION} piersfinlayson/otbiot-amd64:${VERSION}
 docker manifest annotate --arch arm --os linux --variant armv6l piersfinlayson/otbiot:${VERSION} piersfinlayson/otbiot-armv6l:${VERSION}
@@ -17,7 +16,6 @@ docker manifest inspect piersfinlayson/otbiot:${VERSION}
 docker manifest push --purge piersfinlayson/otbiot:${VERSION}
 
 # Create latest manifest
-docker manifest create piersfinlayson/otbiot:latest piersfinlayson/otbiot-amd64:${VERSION} piersfinlayson/otbiot-armv7l:${VERSION}
 docker manifest create piersfinlayson/otbiot:latest piersfinlayson/otbiot-amd64:${VERSION} piersfinlayson/otbiot-armv6l:${VERSION} piersfinlayson/otbiot-armv7l:${VERSION}
 docker manifest annotate --arch amd64 --os linux piersfinlayson/otbiot:latest piersfinlayson/otbiot-amd64:${VERSION}
 docker manifest annotate --arch arm --os linux --variant armv6l piersfinlayson/otbiot:latest piersfinlayson/otbiot-armv6l:${VERSION}
