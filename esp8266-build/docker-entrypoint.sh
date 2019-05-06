8802c1f2bda2fff2019002830fb092e853291822
Piers Finlayson <piers@piersandkatie.com>
2019-05-06 17:49:53 +0100

    add wiringpi to arm, and sudo

--- esp8266-build/docker-entrypoint.sh
+++ esp8266-build/docker-entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 set -e
+echo "Container Version $(cat /VERSION)"
+echo "SDK Version $(find ~/esp-open-sdk/ -name 'ESP*SDK*.zip' -print | awk -F"-" '{ print $4 }' | awk -F"." '{ print $1"."$2 }')"
 cd /home/esp/builds/
 if [ "$1" = 'make' ]; then
-  echo "SDK Version $(cat /VERSION)"
   cd $2 && make $3
 elif [ "$1" = 'bash' ]; then
-  echo "SDK Version $(cat /VERSION)"
   /bin/bash
 else
-  echo "SDK Version $(cat /VERSION)"
   exec "$@"
 fi
