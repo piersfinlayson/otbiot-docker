85518f4ce11f54d01e466000496c23ff0b897c4b
Piers Finlayson <piers@piersandkatie.com>
2020-06-21 20:11:27 +0100

    split out build from upload

---
+++ build/build-container-no-upload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e 
+
+VERSION=$1
+ARG2=$2
+ARCH=$(docker info 2> /dev/null|grep Architecture|awk '{print $2}')
+if [ $ARCH == x86_64 ]
+  then
+    ARCH=amd64
+fi
+
+if [ $ARCH == amd64 ]
+  then
+    PARALLEL_MAKE="-j4"
+else
+  # Run out of RAM doing parallel make on raspberry pis
+  PARALLEL_MAKE=""
+fi
+
+echo "Create piersfinlayson/build container"
+echo "        version: ${VERSION}"
+echo "   architecture: ${ARCH}"
+echo "  PARALLEL_MAKE: ${PARALLEL_MAKE}"
+echo "      other arg: ${ARG2}"
+echo "${VERSION}" > ./VERSION
+
+docker build $ARG2 --build-arg PARALLEL_MAKE=$PARALLEL_MAKE -t piersfinlayson/build-${ARCH}:${VERSION} .
+
+
--- build/build-container.sh
+++ build/build-container.sh
@@ -3,28 +3,17 @@ set -e
 
 VERSION=$1
 ARG2=$2
+./build-container-no-upload.sh $1 $2
 ARCH=$(docker info 2> /dev/null|grep Architecture|awk '{print $2}')
 if [ $ARCH == x86_64 ]
   then
     ARCH=amd64
 fi
 
-if [ $ARCH == amd64 ]
-  then
-    PARALLEL_MAKE="-j4"
-else
-  # Run out of RAM doing parallel make on raspberry pis
-  PARALLEL_MAKE=""
-fi
-
-echo "Create piersfinlayson/build container"
+echo "Upload piersfinlayson/build container"
 echo "        version: ${VERSION}"
 echo "   architecture: ${ARCH}"
-echo "  PARALLEL_MAKE: ${PARALLEL_MAKE}"
-echo "      other arg: ${ARG2}"
-echo "${VERSION}" > ./VERSION
 
-docker build $ARG2 --build-arg PARALLEL_MAKE=$PARALLEL_MAKE -t piersfinlayson/build-${ARCH}:${VERSION} .
 docker login -u piersfinlayson
 docker push piersfinlayson/build-${ARCH}:${VERSION}
 
