c23f184ac78df3e1d38e5f9882972d2e0dc388f8
Piers Finlayson <piers@piersandkatie.com>
2021-12-25 18:11:10 +0000

    add --amend flag just in case

--- build/build-manifest.sh
+++ build/build-manifest.sh
@@ -8,14 +8,14 @@ echo "       version: ${VERSION}"
 docker login
 
 # Create VERSION manifest
-docker manifest create piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv7l:${VERSION}
+docker manifest create -a piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv7l:${VERSION}
 docker manifest annotate --arch amd64 --os linux piersfinlayson/build:${VERSION} piersfinlayson/build-amd64:${VERSION}
 docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/build:${VERSION} piersfinlayson/build-armv7l:${VERSION}
 docker manifest inspect piersfinlayson/build:${VERSION}
 docker manifest push --purge piersfinlayson/build:${VERSION}
 
 # Create latest manifest
-docker manifest create piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv7l:${VERSION}
+docker manifest create -a piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION} piersfinlayson/build-armv7l:${VERSION}
 docker manifest annotate --arch amd64 --os linux piersfinlayson/build:latest piersfinlayson/build-amd64:${VERSION}
 docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/build:latest piersfinlayson/build-armv7l:${VERSION}
 docker manifest inspect piersfinlayson/build:latest
